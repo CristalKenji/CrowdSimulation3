11eb9dc007241fa17743a9995bc2078c429727cb
Umiko <umiko@protonmail.com>
2021-03-01 06:39:57 +0100

    Removed example stuff and made minor code optimizations

---
+++ Assets/Scripts/BoidSettings.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class BoidSettings : ScriptableObject
+{
+    public float AwarenessRadius;
+
+    public float MinVelocity;
+    public float MaxVelocity;
+    public float MaxSteerForce;
+    public bool UseOptimizedRules;
+
+    public float CohesionWeight;
+    public float AlignmentWeight;
+    public float SeparationWeight;
+    public float ObstacleAvoidanceWeight;
+    public LayerMask BoidMask;
+    public int ObstacleRays;
+    public LayerMask ObstacleMask;
+    public float ObstacleDetectionSphereRadius;
+    public float ObstacleDetectionRange;
+    public Material boidMaterialDefault;
+    public Material boidMaterialHeadingForCollision;
+    public Material boidMaterialEscaped;
+
+}
---
+++ Assets/Scripts/BoidSpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidSpawner : MonoBehaviour
+{
+    private static BoidSpawner instance;
+    public static BoidSpawner Instance
+    {
+        get { return instance; }
+    }
+
+
+    [SerializeField]
+    GameObject boidPrefab;
+    [SerializeField]
+    int amountOfBoids = 100;
+    [SerializeField]
+    float spawnRadius = 10;
+
+    internal BoidController[] boids;
+
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int boidCounter = 0; boidCounter < amountOfBoids; boidCounter++)
+        {
+            Instantiate(boidPrefab, transform.position + Random.onUnitSphere * Random.Range(0f, spawnRadius), Random.rotation);
+        }
+        boids = FindObjectsOfType<BoidController>();
+    }
+}
